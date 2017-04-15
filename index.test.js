a5414826cf18786f4cd5b8f7af425eb9bc789df9
Muhammad Habib Rohman <mhrohman@live.com>
2017-04-15 15:38:07 +0700

    add: simple test

---
+++ index.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { join, resolve } from 'path'
+import test from 'ava'
+import S from 'string'
+import { shell, shellSync } from 'execa'
+import pkgJSON from './package.json' // eslint-disable-line import/extensions
+
+const depJSON = pkgJSON.dependencies
+const devDepJSON = pkgJSON.devDependencies
+const depex = join(__dirname, pkgJSON.bin)
+
+const checkDep = (str, dep) => {
+  return Object.keys(dep).map(v => {
+    return S(str).contains(v)
+  })
+}
+
+test.before(t => {
+  // Ask permission
+  shellSync(`chmod +x ${ depex }`)
+})
+
+test.beforeEach(async t => {
+  const noop = await shell(`${ depex }`)
+  t.context.result = noop.stdout
+})
+
+test('should show something', t => {
+  t.truthy(t.context.result)
+})
+
+test('result should contains dependencies', t => {
+  const contains = checkDep(t.context.result, depJSON).indexOf(false) < 0
+  t.true(contains)
+})
+
+test('result should contains devDependencies', t => {
+  const containsDev = checkDep(t.context.result, devDepJSON).indexOf(false) < 0
+  t.true(containsDev)
+})
